0f9752857d3110e7a363dbce949ae617385c3903
JauniKappauni <social.jauni@gmail.com>
2025-02-17 20:27:12 +0100

    Added Configuration to run Flask on 0.0.0.0 at port 20012

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def function1():
-    return render_template("index.html")
+    randomnumb = random.randint(0, 100)
+    output = ""
+    if request.method == "POST":
+        number = int(request.form.get("number"))
+        if number > randomnumb:
+            output = "❌" + "Try again. The number is lower than that"
+        elif number < randomnumb:
+            output = "❌" + "Try again. The number is higher than that"
+        else:
+            output = "✅" + f"{number} was the correct number"
+    return render_template("index.html", output=output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=20012, debug=True)
