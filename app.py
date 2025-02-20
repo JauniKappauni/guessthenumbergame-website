448e960ec39b7dee2a78a617d402da4ae383bac1
JauniKappauni <social.jauni@gmail.com>
2025-02-20 18:59:16 +0100

    Imported os module, to generate a random byte sequence of a length of 16 bytes Imported session variable from flask, to have a persistent randomnumber on every session Added if statement on line 10, to have a persistent random number to be guessed Added if statement on line 16, to be able to delete the random number, which was stored in the session and finally generating a new random number to be guessed, due to limitations in flask, it's not possible to reset the number without refreshing the website, unless AJAX is used Added return of all variables to be able to debug and finally find issues

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import random
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
 @app.route("/", methods=["GET", "POST"])
 def function1():
-    randomnumb = random.randint(0, 100)
+    if "randomnumb" not in session:
+        session["randomnumb"] = random.randint(0, 100)
+    randomnumb = session["randomnumb"]
+    number = 0
     output = ""
     if request.method == "POST":
-        number = int(request.form.get("number"))
-        if number > randomnumb:
-            output = "❌" + "Try again. The number is lower than that"
-        elif number < randomnumb:
-            output = "❌" + "Try again. The number is higher than that"
+        if "reset" in request.form:
+            session.pop("randomnumb", None)
+            session["randomnumb"] = random.randint(0, 100)
+            output = "Number to be guessed was reset. Please refresh the website!"
         else:
-            output = "✅" + f"{number} was the correct number"
-    return render_template("index.html", output=output)
+            number = int(request.form.get("number"))
+            if number > randomnumb:
+                output = "❌ Try again. The number is lower than that"
+            elif number < randomnumb:
+                output = "❌ Try again. The number is higher than that"
+            else:
+                output = "✅ " + f"{number} was the correct number"
+    
+    return render_template("index.html", randomnumb=randomnumb, number=number, output=output)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=20012, debug=True)
